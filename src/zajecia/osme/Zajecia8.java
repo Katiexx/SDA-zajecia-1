838da8065f857024cc55cda37528f3ddcda90706
unknown <k.maczkowska@gmail.com>
2017-02-09 20:43:17 +0100

    Added some String methods

--- src/zajecia/osme/Zajecia8.java
+++ src/zajecia/osme/Zajecia8.java
@@ -1,14 +1,86 @@
 package zajecia.osme;
 
+import zajecia.siodme.Zajecia7;
+
 /**
  * Created by RENT on 2017-02-09.
  */
 public class Zajecia8 {
     public static void main(String[] args) {
-        //System.out.println(factorial(23));
-        // System.out.println(fibonacciNumber(3));
-        //System.out.println(fibonacciNumberRec(6));
+//        String message = switchCase("AddGGTRjjguilI");
+//        System.out.println(message);
+        //System.out.println(switchCaseToBuilder("akjshfTYEF"));
+        //System.out.println(sumOfNumberDigits(3123321));
+//        System.out.println(startsWith1("World is small", "World"));
+//        System.out.println(startsWith2("World is small", "World"));
+//        System.out.println(startsWith3("World is small", "World"));
     }
+
+
+    public static boolean startsWith1(String message, String expression) {
+        return message.startsWith(expression);
+    }
+
+    public static boolean startsWith2(String message, String expression) {
+        return message.substring(0, expression.length()).equals(expression);
+    }
+
+    public static boolean startsWith3(String message, String expression) {
+        char[] messageAsCharArray = message.toCharArray();
+        char[] expressionAsCharArray = expression.toCharArray();
+        boolean flag = true;
+        for (int i = 0; i < expressionAsCharArray.length; i++) {
+            if (messageAsCharArray[i] != expressionAsCharArray[i]) {
+                flag = false;
+            }
+        }
+        return true;
+    }
+
+    public static int sumPOfNumberDigitsWithString(int number) {
+        String numberAsString = String.valueOf(number);
+        return Zajecia7.sumFromString(numberAsString);
+    }
+
+    public static int sumOfNumberDigits(int number) {
+        int sum = 0;
+        while (number != 0) {
+            sum += number % 10;
+            number /= 10;
+        }
+        return sum;
+    }
+
+
+    public static String switchCase(String message) {
+        char[] charArray = message.toCharArray();
+        int number = 1;
+        for (int i = 0; i < charArray.length; i++) {
+            if (charArray[i] >= 65 && charArray[i] <= 90) {
+                charArray[i] += 32;
+            } else if (charArray[i] >= 97 && charArray[i] <= 122) {
+                charArray[i] -= 32;
+            }
+        }
+        return String.valueOf(charArray);
+    }
+
+
+    public static String switchCaseToBuilder(String message) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < message.length(); i++) {
+            char tmp = message.charAt(i);
+            if (tmp > 65 && tmp <= 90) {
+                stringBuilder.append((char) (tmp + 32));
+            } else if (tmp >= 97 && tmp <= 122) {
+                stringBuilder.append((char) (tmp - 32));
+            } else {
+                stringBuilder.append(tmp);
+            }
+        }
+        return stringBuilder.toString();
+    }
+
 
     public static int fibonacciNumberRec(int index) {
         if (index < 0) {
